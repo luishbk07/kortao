11ef67b058b3402f5d02d61904ac49e1905827da
Luis-mltply <117748423+Luis-mltply@users.noreply.github.com>
2026-08-21 17:20:12 -0400

    Update Google AdSense script loading strategy to 'beforeInteractive' for improved ad performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
         </ThemeRegistry>
         <Script
           src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7362041124232949'
-          strategy='afterInteractive'
+          strategy='beforeInteractive'
           crossOrigin='anonymous'
         />
       </body>
